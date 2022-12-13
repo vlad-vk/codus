9bc10cf6118e5a8862e77f056840847da1e76201
vlad-vk <kuzovikk@gmail.com>
2022-12-13 15:08:40 +0200

    Commit W 2022-12-13 13:08:39

--- gksidz/hlp/_cur.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set file=hlp__dip
-
-vkdbfu.exe %file%.dbt c %file%.txt r10
-
-pause
--- gksidz/txt/_cvt.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if exist txt___01.dbt del txt___01.dbt
-if exist txt___02.dbt del txt___02.dbt
-if exist txt___03.dbt del txt___03.dbt
-
-if exist txt___01.txt vkdbfu txt___01.dbt c txt___01.txt r10
-if exist txt___02.txt vkdbfu txt___02.dbt c txt___02.txt r10
-if exist txt___03.txt vkdbfu txt___03.dbt c txt___03.txt r10
